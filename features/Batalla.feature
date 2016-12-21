390e6f96ac4feb8fa95b9e4c608a893cab0f68fd
vagrant <vagrant@kleer-csd-box>
2016-12-21 14:24:18 -0300

    Disparo desde la vista

--- features/Batalla.feature
+++ features/Batalla.feature
@@ -3,4 +3,9 @@ Feature: Juego Batalla Naval
 Scenario: Pantalla con Titulo
 	Given voy a la pantalla
 	Then debo ver "Batalla Naval"
+Scenario: Ingreso Letra A y obtengo Agua
+	Given voy a la pantalla
+	When ingreso Letra "A"
+	Then debo ver "a"
+
 
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,3 +6,16 @@ Then(/^debo ver "(.*?)"$/) do |titulo|
    last_response.body.should =~ /#{titulo}/m
 end
 
+#When(/^ingreso Letra A$/) do |field, value|
+#  fill_in(field, :with => "letra","A")	
+#  click_button("Play")
+#end
+
+When(/^ingreso Letra "(.*?)"$/) do |letra|
+   fill_in("letra", :with => letra)	
+   click_button("Play")
+end
+
+
+
+
