7c3846867ae2da38c0eee346938b37fbabbb1c17
vagrant <vagrant@kleer-csd-box>
2016-12-21 15:08:17 -0300

    se modifico la vista

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require 'sinatra'
 require_relative "./lib/tablero.rb"
 get '/' do
 	@@tablero = Tablero.new
+        @@tablero.agregarBarco(2)
     	erb :Batalla
+end
+post '/disparo' do
+    letra = params["letra"]
+    @resultado = @@tablero.disparo(letra)
+    erb :Batalla
 end
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,10 +6,6 @@ Then(/^debo ver "(.*?)"$/) do |titulo|
    last_response.body.should =~ /#{titulo}/m
 end
 
-#When(/^ingreso Letra A$/) do |field, value|
-#  fill_in(field, :with => "letra","A")	
-#  click_button("Play")
-#end
 
 When(/^ingreso Letra "(.*?)"$/) do |letra|
    fill_in("letra", :with => letra)	
